a5b253dc2630d968e0e895004578f658529da870
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-04-05 11:10:28 +0700

    initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var angka1: UITextField!
+    @IBOutlet weak var angka2: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Result(_ sender: Any) {
+        let a: String? = angka1.text!
+        let b: String? = angka2.text!
+        
+        let hasil = Int(a!)! * Int(b!)!
+        
+        result.text = String(hasil)
+        
+    }
 }
 
